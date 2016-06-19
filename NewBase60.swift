b4ae37214b85b88b5ea7ae3a7519a02460f20e0d
Haroen Viaene <hello@haroen.me>
2016-06-19 18:28:04 +0200

    add date stuff

--- NewBase60.swift
+++ NewBase60.swift
@@ -1,79 +1,111 @@
+import Foundation
+
 //: Converts a Base 10 Integer into Sexagesimal (Base 60) String
 public func IntToSxg(num: Int)->String {
-    var num = num;
-    var sxg = "";
-    let sequence: String = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ_abcdefghijkmnopqrstuvwxyz";
+    var num = num
+    var sxg = ""
+    let sequence: String = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZ_abcdefghijkmnopqrstuvwxyz"
     if (num == 0) {
-        return "0";
+        return "0"
     }
     while (num > 0) {
-        let index: Int = num % 60;
-        sxg = String(sequence[sequence.startIndex.advancedBy(index)]) + sxg;
-        num = (num - index)/60;
+        let index: Int = num % 60
+        sxg = String(sequence[sequence.startIndex.advancedBy(index)]) + sxg
+        num = (num - index)/60
     }
-    return sxg;
+    return sxg
 }
 
 //: Converts a Base 10 Integer to Sexagesimal (Base 60) String of a fixed minimum length
 public func IntToSxgF(num: Int, formatLength: Int = 0)->String {
-    var sxg: String = IntToSxg(num);
-    var zeroesToAdd = formatLength - sxg.characters.count;
+    var sxg: String = IntToSxg(num)
+    var zeroesToAdd = formatLength - sxg.characters.count
     while (zeroesToAdd > 0) {
-        sxg = "0" + sxg;
-        zeroesToAdd = zeroesToAdd - 1;
+        sxg = "0" + sxg
+        zeroesToAdd = zeroesToAdd - 1
     }
-    return sxg;
+    return sxg
 }
 
 //: Converts a Sexageismal (Base 60) String into a Base 10 Integer
 public func SxgToInt(sxg: String)->Int {
-    var num: Int = 0;
+    var num: Int = 0
 
+    sxg
 
     // iterate from first to last char of s
-    for var c in sxg.characters {
+    for var c: UInt8 in sxg.utf8 {
 
-        var i: Int = 0;
+        var i: UInt8 = 0
 
-        if (c>="0" && c<="9") {
-            i = "\(c)".unicodeScalars.first - 48;
+        c
+
+        if (c>=48 && c<=57) {
+            i = c - 48
         }
         else if (c>=65 && c<=72) {
-            c-=55;
+            i = c - 55
         }
 
         // typo capital I,
         // lowercase l to 1
         else if (c==73 || c==108) {
-            c=1;
+            i = 1
         }
         else if (c>=74 && c<=78) {
-            c-=56;
+            i = c - 56
         }
 
         // error correct typo capital O to 0
-        else if (c==79) { c=0; }
+        else if (c==79) { c=0 }
         else if (c>=80 && c<=90) {
-            c-=57;
+            i = c - 57
         }
 
         // underscore
-        else if (c==95) { c=34; }
+        else if (c==95) {
+            c=34
+        }
         else if (c>=97 && c<=107) {
-            c-=62;
+            i = c - 62
         }
         else if (c>=109 && c<=122) {
-            c-=63;
+            i = c - 63
         }
 
-        // treat all other noise as 0
-        else { c = 0; }
-        num = (60 * num) + i;
+        num = (60 * num) + Int(i)
     }
-    return num;
+    return num
 }
 
 
-IntToSxg(40)
 
-SxgToInt("f")
+func DateToSxg(date: NSDate)->String {
+    let sinceEpoch = date.timeIntervalSince1970
+    let epochDays = Int(floor(sinceEpoch / (60 * 60 * 24)))
+
+    return IntToSxg(epochDays)
+}
+
+func DateToSxgF(date: NSDate, formatLength: Int = 0)->String {
+    let sinceEpoch = date.timeIntervalSince1970
+
+    let epochDays = Int(floor(sinceEpoch / (60 * 60 * 24)))
+
+    return IntToSxgF(epochDays, formatLength: formatLength)
+}
+
+func SxgToDate(sxg: String)->NSDate {
+    let num = SxgToInt(sxg)
+    let interval: NSTimeInterval = NSTimeInterval(num * 60 * 60 * 24)
+
+    return NSDate(timeIntervalSince1970: interval)
+}
+
+IntToSxg(8645997)
+
+SxgToInt("flex")
+
+DateToSxg(NSDate())
+
+SxgToDate("4hr")
